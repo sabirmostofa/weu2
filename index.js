3dfc8f6a365ed3c0a8fc652d8a6098243748992a
sabirmostofa <sabirmostofa@gmail.com>
2017-01-25 06:49:06 +0100

    2nd

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser')
-var DataStorage = require("./DataStorage");
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = 3000;
-var dataStorage = new DataStorage("todoItems.json");
+
 var fs = require("fs");
 
 app.use(bodyParser.json());
